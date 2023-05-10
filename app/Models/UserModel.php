ae56f040f0a23561eec3b49e3cf9b7792bf9c1a7
Rafly Andrian <raflyandrian@gmail.com>
2023-05-10 20:34:22 +0700

    now show() only passed decoded email instead of token, now decode is happening in controller to be able to use try catch

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -134,32 +134,22 @@ class UserModel extends Model
         }
     }
 
-    public function show($token)
+    public function show($email)
     {
         $session = \Config\Services::session();
         $model = new UserModel();
-        helper('jwt');
-        try {
-            $decoded_token = validateJWT($_COOKIE['COOKIE-SESSION']);
-            $builder = $this->table('users');
-            $data = $builder->where('email', $decoded_token->email)->first();
-            if (!$data) {
-                return false;
-            } else {
-                $data2 = [
-                    'email' => $data['email'],
-                    'name' => $data['name'],
-                    'image' => $data['image'],
-                    'date_created' => $data['date_created']
-                ];
-                return $data2;
-            }
-        } catch (Exception $e) {
-            $session->setFlashdata('message', '<div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-300 dark:bg-gray-800 dark:text-red-400" role="alert">
-                <span class="font-medium">Session Expired</span>, Please login again
-              </div>');
-            // echo $e->getMessage();
-            return redirect()->to('/auth');
+        $builder = $this->table('users');
+        $data = $builder->where('email', $email)->first();
+        if (!$data) {
+            return false;
+        } else {
+            $data2 = [
+                'email' => $data['email'],
+                'name' => $data['name'],
+                'image' => $data['image'],
+                'date_created' => $data['date_created']
+            ];
+            return $data2;
         }
     }
 }
