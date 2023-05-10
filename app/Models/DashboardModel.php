bd2c0d944b6ca87262b52414ce3f8780fcf361b5
Rafly Andrian <raflyandrian@gmail.com>
2023-05-10 14:13:26 +0700

    Remove show() from DashboardModel and place it in UserModel

--- app/Models/DashboardModel.php
+++ app/Models/DashboardModel.php
@@ -11,35 +11,4 @@ class DashboardModel extends Model
 {
     protected $table            = 'users';
     protected $primaryKey       = 'id';
-
-    public function show($token)
-    {
-        $session = \Config\Services::session();
-        $model = new DashboardModel();
-        helper('jwt');
-        try {
-            $decoded_token = validateJWT($_COOKIE['COOKIE-SESSION']);
-            $builder = $this->table('users');
-            $data = $builder->where('email', $decoded_token->email)->first();
-            if (!$data) {
-                return false;
-            } else {
-                $data2 = [
-                    'email' => $data['email'],
-                    'name' => $data['name'],
-                    'image' => $data['image'],
-                    'date_created' => $data['date_created']
-                ];
-                return $data2;
-            }
-        } catch (Exception $e) {
-            $session->setFlashdata('message', '<div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-300 dark:bg-gray-800 dark:text-red-400" role="alert">
-                <span class="font-medium">Session Expired</span>, Please login again
-              </div>');
-            // echo $e->getMessage();
-            return redirect()->to('/auth');
-        }
-
-        // $decoded_token = JWT::decode($_COOKIE['COOKIE-SESSION'], new Key($key, 'HS256'));
-    }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -133,4 +133,33 @@ class UserModel extends Model
             return false;
         }
     }
+
+    public function show($token)
+    {
+        $session = \Config\Services::session();
+        $model = new UserModel();
+        helper('jwt');
+        try {
+            $decoded_token = validateJWT($_COOKIE['COOKIE-SESSION']);
+            $builder = $this->table('users');
+            $data = $builder->where('email', $decoded_token->email)->first();
+            if (!$data) {
+                return false;
+            } else {
+                $data2 = [
+                    'email' => $data['email'],
+                    'name' => $data['name'],
+                    'image' => $data['image'],
+                    'date_created' => $data['date_created']
+                ];
+                return $data2;
+            }
+        } catch (Exception $e) {
+            $session->setFlashdata('message', '<div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-300 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <span class="font-medium">Session Expired</span>, Please login again
+              </div>');
+            // echo $e->getMessage();
+            return redirect()->to('/auth');
+        }
+    }
 }
