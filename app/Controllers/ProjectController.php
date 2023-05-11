2e54e2c40fb54c6aae2f331e0e6c8df6d2b15692
Rafly Andrian <raflyandrian@gmail.com>
2023-05-11 12:29:08 +0700

    Fix the displaying variable, now function only return $data, no redirect

--- app/Controllers/ProjectController.php
+++ app/Controllers/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\ProjectModel;
 use App\Models\UserModel;
+use CodeIgniter\Router\Exceptions\RedirectException;
 
 class ProjectController extends BaseController
 {
@@ -19,43 +20,81 @@ class ProjectController extends BaseController
             $session->setFlashdata('message', 'Session expired, Please login again');
             return view('login', $data);
         } else {
-            $session = \Config\Services::session();
-            $token = $_COOKIE['COOKIE-SESSION'];
-            $userModel = new UserModel();
-            $projectModel = new ProjectModel();
-            helper('jwt');
-            //decode header token jwt
-            $decoded_token = validateJWT($token);
-            $user = $userModel->show($decoded_token->email);
-            //check if user correct with token payload
-            if (!$user) {
-                $data['title'] = 'Login';
-                $session->setFlashdata('message', 'Session expired, Please login again');
-                return view('login', $data);
-            } else {
-                $result = $projectModel->showData();
-                $client = $projectModel->getClient();
-                if (!$result && !$client) {
-                    echo 'Database empty';
-                } else {
-                    $data = [
-                        'title' => 'Project Data',
-                        'project' => $result->getResult(),
-                        'client' => $client->getResult(),
-                        'email' => $user['email'],
-                        'name' => $user['name'],
-                        'image' => $user['image'],
-                        'date_created' => $user['date_created']
-                    ];
-                    // dd($data);
-                    return view('project', $data);
-                }
-            }
-            $projectModel->getClient();
+            $user = $this->user();
+            $table = $this->showTable();
+            $data = [
+                'title' => $user['title'],
+                'name' => $user['name'],
+                'image' => $user['image'],
+                'project' => $table['project'],
+                'client' => $table['client'],
+            ];
+            return view('project', $data);
         }
     }
 
-    public function show()
+    public function user()
     {
+        $session = \Config\Services::session();
+        $token = $_COOKIE['COOKIE-SESSION'];
+        $userModel = new UserModel();
+        $projectModel = new ProjectModel();
+        helper('jwt');
+        //decode header token jwt
+        $decoded_token = validateJWT($token);
+        try {
+            $user = $userModel->show($decoded_token->email);
+        } catch (RedirectException $e) {
+            $data['title'] = 'Login';
+            $session->setFlashdata('message', 'Session expired, Please login again');
+            return view('login', $data);
+        }
+        //check if user correct with token payload
+        if (!$user) {
+        } else {
+            $data = [
+                'title' => 'Project Data',
+                'name' => $user['name'],
+                'image' => $user['image'],
+            ];
+            return $data;
+        }
+    }
+
+    public function showTable()
+    {
+        $projectModel = new ProjectModel();
+        $result = $projectModel->showData();
+        if (!$result) {
+            //Flashdata here
+        } else {
+            $client = $projectModel->getClient();
+            $data = [
+                'project' => $result->getResult(),
+                'client' => $client->getResult(),
+            ];
+            return $data;
+        }
     }
 }
+
+
+
+
+
+
+// if (!$result && !$client) {
+//     echo 'Database empty';
+// } else {
+//     $data = [
+//         'title' => 'Project Data',
+//         'project' => $result->getResult(),
+//         'client' => $client->getResult(),
+//         'email' => $user['email'],
+//         'name' => $user['name'],
+//         'image' => $user['image'],
+//         'date_created' => $user['date_created']
+//     ];
+//     // dd($data);
+//     return view('project', $data);
+// }
