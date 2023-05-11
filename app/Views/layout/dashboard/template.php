50f4b2b54db0cdf2d8a4a07fea85164cc1ef82e0
Rafly Andrian <raflyandrian@gmail.com>
2023-05-11 11:21:51 +0700

    changed title

--- app/Views/layout/dashboard/template.php
+++ app/Views/layout/dashboard/template.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dashboard</title>
+    <title><?= $title; ?></title>
 
 
     <!-- Custom fonts for this template-->
