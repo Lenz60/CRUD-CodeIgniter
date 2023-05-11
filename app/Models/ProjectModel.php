24a6eb1400bcaabfb2ae18aa1419da1c1258546b
Rafly Andrian <raflyandrian@gmail.com>
2023-05-11 12:29:30 +0700

    Make function for filtering

--- app/Models/ProjectModel.php
+++ app/Models/ProjectModel.php
@@ -22,6 +22,37 @@ class ProjectModel extends Model
         return $data;
     }
 
+    public function showDataWhere($data)
+    {
+        $projectName = $data['project_name'];
+        $clientId = $data['client_id'];
+        $projectStatus = $data['project_status'];
+        $builder = $this->table('tb_m_project');
+        $builder->select('Project.project_name, Client.client_name, Project.project_start, Project.project_end, Project.project_status');
+        $builder->from('tb_m_project as Project');
+        $builder->join('tb_m_client as Client', 'Client.client_id = Project.client_id');
+        if ($projectName == "" || $projectName == null) {
+            if ($clientId == 'all') {
+                $builder->where('Project.project_status', $projectStatus);
+            } else {
+                $builder->where('Client.client_id', $clientId);
+                $builder->where('Project.project_status', $projectStatus);
+            }
+        } else {
+            if ($clientId == 'all') {
+                $builder->where('Project.project_name', $projectName);
+                $builder->where('Project.project_status', $projectStatus);
+            } else {
+                $builder->where('Project.project_name', $projectName);
+                $builder->where('Client.client_id', $clientId);
+                $builder->where('Project.project_status', $projectStatus);
+            }
+        }
+        $builder->groupBy('Project.project_id');
+        $data = $builder->get();
+        return $data;
+    }
+
     public function getClient()
     {
         $builder = $this->table('tb_m_client');
