56e564b73e05fed80178680a5b597406753c19a1
Rafly Andrian <raflyandrian@gmail.com>
2023-05-11 11:21:37 +0700

    move the show to top

--- app/Controllers/ProjectController.php
+++ app/Controllers/ProjectController.php
@@ -11,64 +11,51 @@ class ProjectController extends BaseController
 
     public function index()
     {
-        //
         $session = \Config\Services::session();
+        //check session
+        //if no session exists return login
         if (!isset($_COOKIE['COOKIE-SESSION'])) {
             $data['title'] = 'Login';
             $session->setFlashdata('message', 'Session expired, Please login again');
             return view('login', $data);
         } else {
-            $model = new ProjectModel();
-            $model->getClient();
-            $this->showData();
-            // $this->show();
-            return view('project');
+            $session = \Config\Services::session();
+            $token = $_COOKIE['COOKIE-SESSION'];
+            $userModel = new UserModel();
+            $projectModel = new ProjectModel();
+            helper('jwt');
+            //decode header token jwt
+            $decoded_token = validateJWT($token);
+            $user = $userModel->show($decoded_token->email);
+            //check if user correct with token payload
+            if (!$user) {
+                $data['title'] = 'Login';
+                $session->setFlashdata('message', 'Session expired, Please login again');
+                return view('login', $data);
+            } else {
+                $result = $projectModel->showData();
+                $client = $projectModel->getClient();
+                if (!$result && !$client) {
+                    echo 'Database empty';
+                } else {
+                    $data = [
+                        'title' => 'Project Data',
+                        'project' => $result->getResult(),
+                        'client' => $client->getResult(),
+                        'email' => $user['email'],
+                        'name' => $user['name'],
+                        'image' => $user['image'],
+                        'date_created' => $user['date_created']
+                    ];
+                    // dd($data);
+                    return view('project', $data);
+                }
+            }
+            $projectModel->getClient();
         }
     }
 
     public function show()
     {
-    }
-
-    public function showData()
-    {
-
-        $session = \Config\Services::session();
-        $token = $_COOKIE['COOKIE-SESSION'];
-        $userModel = new UserModel();
-        $projectModel = new ProjectModel();
-        helper('jwt');
-        try {
-            $decoded_token = validateJWT($token);
-            $user = $userModel->show($decoded_token->email);
-        } catch (Exception $e) {
-            $session->setFlashdata('message', '<div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-300 dark:bg-gray-800 dark:text-red-400" role="alert">
-            <span class="font-medium">Session Expired</span>, Please login again
-            </div>');
-            return redirect()->to('/auth');
-        }
-        if (!$user) {
-            $data['title'] = 'Login';
-            $session->setFlashdata('message', 'Session expired, Please login again');
-            return view('login', $data);
-        } else {
-            $result = $projectModel->showData();
-            $client = $projectModel->getClient();
-            if (!$result && !$client) {
-                echo 'Database empty';
-            } else {
-                $data = [
-                    'title' => 'Project Data',
-                    'project' => $result->getResult(),
-                    'client' => $client->getResult(),
-                    'email' => $user['email'],
-                    'name' => $user['name'],
-                    'image' => $user['image'],
-                    'date_created' => $user['date_created']
-                ];
-                // dd($data);
-                return view('project', $data);
-            }
-        }
     }
 }
